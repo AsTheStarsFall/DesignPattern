789e30be814d66c5cb298ed79eb276ce8fd9a610
AsTheStarsFall <1295029087@qq.com>
2019-03-22 06:14:05 +0800

    test

---
+++ src/main/java/com/tianhy/designpattern/observer/guava/GupaoTest.java
@@ -0,0 +1,26 @@
+package com.tianhy.designpattern.observer.guava;
+
+import com.google.common.eventbus.EventBus;
+
+/**
+ * @Desc: 测试类
+ * @Author: thy
+ * @CreateTime: 2019/3/22
+ **/
+public class GupaoTest {
+    public static void main(String[] args) {
+        //发布者
+        Publisher publisher = new Publisher(new EventBus());
+        //订阅者
+        Observer tom = new Observer("Tom老师");
+        Observer mic = new Observer("Mic老师");
+        //内容
+        publisher.setPublisherName("咕泡学员");
+        publisher.setQuestion("请问怎么才能变帅？");
+        //注册
+        publisher.register(tom);
+        publisher.register(mic);
+        //发布
+        publisher.post(publisher);
+    }
+}
