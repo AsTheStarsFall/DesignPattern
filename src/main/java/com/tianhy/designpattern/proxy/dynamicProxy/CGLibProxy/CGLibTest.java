8cba478f46229b42c0965f9eb97b66417f71d56e
AsTheStarsFall <1295029087@qq.com>
2019-04-09 17:12:18 +0800

    CGlib

---
+++ src/main/java/com/tianhy/designpattern/proxy/dynamicProxy/CGLibProxy/CGLibTest.java
@@ -0,0 +1,14 @@
+package com.tianhy.designpattern.proxy.dynamicProxy.CGLibProxy;
+
+/**
+ * @Desc:
+ * @Author: thy
+ * @CreateTime: 2019/3/25
+ **/
+public class CGLibTest {
+
+    public static void main(String[] args) {
+        Custom custom = (Custom) new Agency().getInstance(Custom.class);
+        custom.finHouse();
+    }
+}
